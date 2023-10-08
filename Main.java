fea2b4a9cbcde9b6da0e6fe4e8f1a0b155039d4a
aish-10 <ambwaniaishwarya@gmail.com>
2023-10-08 22:54:29 +0530

    correction in if else

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
             System.out.println("a is greater than b");
         }
         else{
-            System.out.println("b is greater than a");
+            System.out.println("a is lesser than b");
         }
 
     }
